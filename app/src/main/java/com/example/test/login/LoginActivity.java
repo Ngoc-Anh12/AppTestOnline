7430e964e989a83fb3849325ce0e4a283e3b27c0
mistergentle <longnguyen20121998@gmail.com>
2021-03-27 00:51:46 +0700

    add new

--- app/src/main/java/com/example/test/login/LoginActivity.java
+++ app/src/main/java/com/example/test/login/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,76 +25,77 @@ import java.util.concurrent.Executor;
 import javax.crypto.Cipher;
 
 public class LoginActivity extends AppCompatActivity {
-    ImageButton imageButtonBack;
-    AppCompatButton btnLogin;
+  ImageButton imageButtonBack;
+  AppCompatButton btnLogin;
+  ImageView fingerprint_image;
 
-    private androidx.biometric.BiometricPrompt biometricPrompt;
-    private androidx.biometric.BiometricPrompt.PromptInfo promptInfo;
-    GFingerPrint gFingerPrint;
-    private Executor executor;
+  private androidx.biometric.BiometricPrompt biometricPrompt;
+  private androidx.biometric.BiometricPrompt.PromptInfo promptInfo;
+  GFingerPrint gFingerPrint;
+  private Executor executor;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        init();
-        handle();
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    init();
+    handle();
+  }
 
-    private void handle() {
-        imageButtonBack.setOnClickListener(view ->{
-            finish();
-        });
+  private void handle() {
+    imageButtonBack.setOnClickListener(view ->{
+      finish();
+    });
 
-        btnLogin.setOnClickListener(view ->{
-            Intent intentHome = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(intentHome);
-        });
-    }
+    btnLogin.setOnClickListener(view ->{
+      Intent intentHome = new Intent(LoginActivity.this, HomeActivity.class);
+      startActivity(intentHome);
+    });
+  }
 
-    public void settingFingerPrint(){
-        gFingerPrint = new GFingerPrint(this);
-        executor = ContextCompat.getMainExecutor(this);
-        biometricPrompt = new BiometricPrompt(this , executor, new BiometricPrompt.AuthenticationCallback(){
-            @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-                Toast.makeText( LoginActivity.this,
-                        "Lỗi vân tay: " + errString, Toast.LENGTH_SHORT)
-                        .show();
-            }
+  public void settingFingerPrint(){
+    gFingerPrint = new GFingerPrint(this);
+    executor = ContextCompat.getMainExecutor(this);
+    biometricPrompt = new BiometricPrompt(this , executor, new BiometricPrompt.AuthenticationCallback(){
+      @Override
+      public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+        super.onAuthenticationError(errorCode, errString);
+        Toast.makeText( LoginActivity.this,
+            "Lỗi vân tay: " + errString, Toast.LENGTH_SHORT)
+            .show();
+      }
 
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
+      @RequiresApi(api = Build.VERSION_CODES.M)
+      @Override
+      public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+        super.onAuthenticationSucceeded(result);
 
 
-                Cipher cipher =  result.getCryptoObject().getCipher();
-                String encryptPassword = gFingerPrint.encryptPassword(cipher, AppData.getInstance().password);
+        Cipher cipher =  result.getCryptoObject().getCipher();
+        String encryptPassword = gFingerPrint.encryptPassword(cipher, AppData.getInstance().password);
 
-                DataService.getInstance(null).storeFingerPassword(AppData.getInstance().userUUID,encryptPassword);
-                DataService.getInstance(null).save();
-            }
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-            }
-        });
-        promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Sử dụng vân tay để đăng nhập")
-                .setNegativeButtonText("Không sử dụng vân tay")
-                .build();
+        DataService.getInstance(null).storeFingerPassword(AppData.getInstance().userUUID,encryptPassword);
+        DataService.getInstance(null).save();
+      }
+      @Override
+      public void onAuthenticationFailed() {
+        super.onAuthenticationFailed();
+      }
+    });
+    promptInfo = new BiometricPrompt.PromptInfo.Builder()
+        .setTitle("Sử dụng vân tay để đăng nhập")
+        .setNegativeButtonText("Không sử dụng vân tay")
+        .build();
 
-    }
+  }
 
-    private void init() {
-        imageButtonBack = findViewById(R.id.image_button_back);
-        btnLogin = findViewById(R.id.btn_login);
-    }
+  private void init() {
+    imageButtonBack = findViewById(R.id.image_button_back);
+    btnLogin = findViewById(R.id.btn_login);
+  }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
+  @Override
+  public void onBackPressed() {
+    super.onBackPressed();
+  }
 }
